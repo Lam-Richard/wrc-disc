2b0df4fad813f6c53982dfde8537d39cec9a609f
Lam-Richard <richardlam2024@gmail.com>
2021-02-13 14:56:45 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
 
+/*
+Create a bunch of dummy data and list them and show them in a list
+*/
+
+/*
+shift: can be either "D" "N", 0 or 1.
+englishLevel: 1-5 -> maps to e.g. profiient, not etc.
+location: a string
+*/
+
+//Object.keys(<object name>)
+
+const jobRequirements = {
+  walmart: {
+    company: 'Walmart',
+    englishLevel: 3,
+    location: "Chicago",
+    shift: 0
+  },
+  cvs: {
+    company: 'CVS',
+    englishLevel: 4,
+    location: "Chicago",
+    shift: 0,
+  },
+  walgreens: {
+    company: 'Walgreens',
+    englishLevel: 2,
+    location: "Evanston",
+    shift: 1
+  }
+};
+
+const Listings = () => {
+  return (
+    Object.values(jobRequirements).map((job)=> {
+      return (
+        <div style={{border: "red 1px solid", 
+        height: 50, width: 300, marginBottom: '5px',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        
+        }}>
+          <div style={{textAlign: 'center'}}>{job.company}: {job.location}, {job.shift == 0 ? "Day" : "Night"}</div>
+        </div>
+      )
+    })
+  )
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className="App">
+      <Listings></Listings>
     </div>
   );
 }
